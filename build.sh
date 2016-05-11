a3b96e1afa3f861568190918b65e73bfafc4cdcf
Ingy döt Net <ingy@ingy.net>
2016-05-11 15:22:01 -0700

    Remove dead code

--- build.sh
+++ build.sh
@@ -22,11 +22,6 @@ set -x
   cpanm -n -f Tickit::Async
   yes | cpanm -n --prompt Task::Tickit::Widget
 
-  # wget https://cpan.metacpan.org/authors/id/P/PE/PEVANS/Tickit-0.56.tar.gz
-  # tar xzf Tickit-0.56.tar.gz
-  # cp Tickit-0.56/examples/*.pl /
-  # grep SYNOPSIS -A18 Tickit-0.56/README | tail -n 18 > /demo-hello-world.pl
-
   if $finalize; then
     apk del $apks
     apk del $apks_dev
